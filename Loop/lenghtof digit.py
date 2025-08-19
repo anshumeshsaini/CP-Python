914cecaf6d516463b4e02bb5bbf5e37979aa698a
Anshumesh saini <anshumesh.saini@gmail.com>
2025-08-19 10:15:25 +0530

    Initial commit

---
+++ Loop/lenghtof digit.py
@@ -0,0 +1,3 @@
+n = int(input("enter an integer "))
+length = len(str(abs(n)))
+print("number of digits", length)
---
+++ print Days Using 1-7 numbers.py
@@ -0,0 +1,21 @@
+day=int(input("Enter the number with 1 to 7"))
+if day==1:
+    print("Sunday")
+elif day==2:
+    print("monday")
+elif day==3:
+    print("tuesday")
+elif day==4:
+    print("wendsday")
+elif day==5:
+    print("thursday")
+
+elif day==6:
+    print("friday")
+elif day==7:
+    print("sat")
+
+else:
+    print("wrong inpiut")
+
+
