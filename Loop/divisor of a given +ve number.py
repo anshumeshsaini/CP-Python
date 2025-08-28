164617eabfc0a5bf68b9bd42161692eaf03f053c
Anshumesh Saini <119862734+anshumeshsaini@users.noreply.github.com>
2025-08-29 00:14:37 +0530

    Delete Loop directory

--- Loop/divisor of a given +ve number.py
+++
@@ -1,7 +0,0 @@
-a = int(input("enter a number + number "))
-divisor =[]
-for i in range(1,a+1):
-    if a%i==0:
-        divisor.append(i)
-        print("divisor",a,":",*divisor)
-
--- Loop/lenghtof digit.py
+++
@@ -1,3 +0,0 @@
-n = int(input("enter an integer "))
-length = len(str(abs(n)))
-print("number of digits", length)
