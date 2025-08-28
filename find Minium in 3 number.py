3ca7546b63a5a230f44a2571e513a89cb470d318
Anshumesh Saini <119862734+anshumeshsaini@users.noreply.github.com>
2025-08-29 00:15:42 +0530

    Delete find Minium in 3 number.py

--- find Minium in 3 number.py
+++
@@ -1,9 +0,0 @@
-a= float(input("enter the 1st number"))
-b= float(input("Enter the 2nd number"))
-c=float(input("Enter the 3rd number"))
-if a<=b and a<=c:
-    print(f"the mini is {a}")
-elif b<=a and b<=c:
-    print(f"the mini is {b}")
-else:
-    print(f"{c}")
