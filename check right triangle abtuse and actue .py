abebef8b09a3b14c8ec3935f313a59478e89b8bf
Anshumesh Saini <119862734+anshumeshsaini@users.noreply.github.com>
2025-08-29 00:15:34 +0530

    Delete check right triangle abtuse and actue .py

--- check right triangle abtuse and actue .py
+++
@@ -1,9 +0,0 @@
-a = int(input("enter the 1st number"))
-b = int(input("enter the 2st number"))
-c = int(input("enter the 3st number"))
-if a ==90 or b==90 or c==90:
-    print("right triangle")
-elif a>90 or b >90 or c>90:
-    print("obtuse tringale")
-else :
-    print("actue angle ")
